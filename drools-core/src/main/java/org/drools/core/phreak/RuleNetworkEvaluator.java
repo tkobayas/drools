apache/incubator-kie-drools

[DROOLS-7528] Avoid casting to unnecessary interfaces (Fix the secondary super cache problem in Drools) (#5451)
* [DROOLS-7528] Avoid casting to unnecessary interfaces (Fix the secondary super cache problem in Drools)

* Reintroduce fix in LeftTuple

* The TupleSink is not always a BetaNode

* Removed leftover

drools-core/src/main/java/org/drools/core/phreak/RuleNetworkEvaluator.java
@@ -25,6 +25,7 @@ import org.drools.base.common.NetworkNode;
 import org.drools.core.common.ReteEvaluator;
 import org.drools.core.common.TupleSets;
 import org.drools.core.common.TupleSetsImpl;
+import org.drools.core.reteoo.AbstractTerminalNode;
 import org.drools.core.reteoo.AccumulateNode;
 import org.drools.core.reteoo.AccumulateNode.AccumulateMemory;
 import org.drools.core.reteoo.AsyncReceiveNode;
@@ -327,7 +328,7 @@ public class RuleNetworkEvaluator {
             boolean terminalNode = true;
             switch (node.getType()) {
                 case NodeTypeEnums.RuleTerminalNode:
-                    pRtNode.doNode(( TerminalNode ) node, activationsManager, srcTuples, executor);
+                    pRtNode.doNode((AbstractTerminalNode) node, activationsManager, srcTuples, executor);
                     break;
                 case NodeTypeEnums.QueryTerminalNode:
                     pQtNode.doNode((QueryTerminalNode) node, activationsManager, srcTuples, stack);

tkobayas/drools

(cherry picked from commit abbfa6f7d7b73fa29773746d2debc5e9fc3bc172)

drools-core/src/main/java/org/drools/core/phreak/RuleNetworkEvaluator.java
@@ -25,6 +25,7 @@ import org.drools.core.common.Memory;
 import org.drools.core.common.NetworkNode;
 import org.drools.core.common.TupleSets;
 import org.drools.core.common.TupleSetsImpl;
+import org.drools.core.reteoo.AbstractTerminalNode;
 import org.drools.core.reteoo.AccumulateNode;
 import org.drools.core.reteoo.AccumulateNode.AccumulateMemory;
 import org.drools.core.reteoo.AsyncReceiveNode;
@@ -325,7 +326,7 @@ public class RuleNetworkEvaluator {
             boolean terminalNode = true;
             switch (node.getType()) {
                 case NodeTypeEnums.RuleTerminalNode:
-                    pRtNode.doNode(( TerminalNode ) node, agenda, srcTuples, executor);
+                    pRtNode.doNode(( AbstractTerminalNode ) node, agenda, srcTuples, executor);
                     break;
                 case NodeTypeEnums.QueryTerminalNode:
                     pQtNode.doNode((QueryTerminalNode) node, agenda, srcTuples, stack);