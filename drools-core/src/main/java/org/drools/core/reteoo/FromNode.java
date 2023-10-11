apache/incubator-kie-drools

[DROOLS-7527] Alyways use AbstractTuple instead of Tuple (Fix the secondary super cache problem in Drools) (#5447)
* First commit removing Tuple for AbstractTuple as next

* Cast to AbstractTuple in the FromNode

drools-core/src/main/java/org/drools/core/reteoo/FromNode.java
@@ -225,7 +225,7 @@ public class FromNode<T extends FromNode.FromMemory> extends LeftTupleSource
                 // this is for the obscene case where two or more objects returned by "from"
                 // have the same hash code and evaluate equals() to true, so we need to preserve
                 // all of them to avoid leaks
-                rightTuple.setNext( existingMatch );
+                rightTuple.setNext((AbstractTuple) existingMatch);
             }
             matches.put( object,
                          rightTuple );

drools-core/src/main/java/org/drools/core/util/index/TupleList.java
@@ -19,6 +19,7 @@ package org.drools.core.util.index;
 import java.io.Serializable;
 
 import org.drools.core.common.InternalFactHandle;
+import org.drools.core.reteoo.AbstractTuple;
 import org.drools.core.reteoo.TupleMemory;
 import org.drools.core.reteoo.Tuple;
 import org.drools.core.util.Entry;
@@ -85,8 +86,8 @@ public class TupleList<C> implements TupleMemory, Entry<TupleList<C>>, Serializa
             return;
         }
 
-        Tuple previous = tuple.getPrevious();
-        Tuple next = tuple.getNext();
+        AbstractTuple previous = (AbstractTuple) tuple.getPrevious();
+        AbstractTuple next = tuple.getNext();
         if (previous == null) {
             next.setPrevious( null );
             this.first = next;
@@ -95,7 +96,7 @@ public class TupleList<C> implements TupleMemory, Entry<TupleList<C>>, Serializa
             next.setPrevious( previous );
         }
 
-        this.last.setNext( tuple );
+        this.last.setNext((AbstractTuple) tuple);
         tuple.setPrevious( this.last );
         tuple.setNext( null );
         this.last = tuple;
@@ -103,7 +104,7 @@ public class TupleList<C> implements TupleMemory, Entry<TupleList<C>>, Serializa
 
     public void add(final Tuple tuple) {
         if ( this.last != null ) {
-            this.last.setNext( tuple );
+            this.last.setNext( (AbstractTuple) tuple );
             tuple.setPrevious( this.last );
             this.last = tuple;
         } else {
@@ -117,7 +118,7 @@ public class TupleList<C> implements TupleMemory, Entry<TupleList<C>>, Serializa
 
     public void remove(final Tuple tuple) {
         Tuple previous = tuple.getPrevious();
-        Tuple next = tuple.getNext();
+        AbstractTuple next = tuple.getNext();
 
         if ( previous != null && next != null ) {
             // remove from middle

tkobayas/drools

(cherry picked from commit f83ff413b0bf0f6a047190901d5415299b7f198a)

drools-core/src/main/java/org/drools/core/reteoo/FromNode.java
@@ -252,7 +252,7 @@ public class FromNode<T extends FromNode.FromMemory> extends LeftTupleSource
                 // this is for the obscene case where two or more objects returned by "from"
                 // have the same hash code and evaluate equals() to true, so we need to preserve
                 // all of them to avoid leaks
-                rightTuple.setNext( existingMatch );
+                rightTuple.setNext((BaseTuple) existingMatch);
             }
             matches.put( object,
                          rightTuple );

drools-core/src/main/java/org/drools/core/util/index/TupleList.java
@@ -19,6 +19,7 @@ package org.drools.core.util.index;
 import java.io.Serializable;
 
 import org.drools.core.common.InternalFactHandle;
+import org.drools.core.reteoo.BaseTuple;
 import org.drools.core.reteoo.TupleMemory;
 import org.drools.core.spi.Tuple;
 import org.drools.core.util.Entry;
@@ -86,8 +87,8 @@ public class TupleList<C> implements TupleMemory, Entry<TupleList<C>>, Serializa
             return;
         }
 
-        Tuple previous = tuple.getPrevious();
-        Tuple next = tuple.getNext();
+        BaseTuple previous = (BaseTuple) tuple.getPrevious();
+        BaseTuple next = tuple.getNext();
         if (previous == null) {
             next.setPrevious( null );
             this.first = next;
@@ -96,7 +97,7 @@ public class TupleList<C> implements TupleMemory, Entry<TupleList<C>>, Serializa
             next.setPrevious( previous );
         }
 
-        this.last.setNext( tuple );
+        this.last.setNext((BaseTuple) tuple);
         tuple.setPrevious( this.last );
         tuple.setNext( null );
         this.last = tuple;
@@ -104,7 +105,7 @@ public class TupleList<C> implements TupleMemory, Entry<TupleList<C>>, Serializa
 
     public void add(final Tuple tuple) {
         if ( this.last != null ) {
-            this.last.setNext( tuple );
+            this.last.setNext( (BaseTuple) tuple );
             tuple.setPrevious( this.last );
             this.last = tuple;
         } else {
@@ -118,7 +119,7 @@ public class TupleList<C> implements TupleMemory, Entry<TupleList<C>>, Serializa
 
     public void remove(final Tuple tuple) {
         Tuple previous = tuple.getPrevious();
-        Tuple next = tuple.getNext();
+        BaseTuple next = tuple.getNext();
 
         if ( previous != null && next != null ) {
             // remove from middle