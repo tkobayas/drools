ad269909a4d22f518593c048914deede637eec5f
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2026-09-01 17:54:07 +0900

    retrigger

--- kogito-addons/common/persistence/jdbc/src/main/java/org/kie/kogito/persistence/jdbc/AbstractProcessInstancesFactory.java
+++ kogito-addons/common/persistence/jdbc/src/main/java/org/kie/kogito/persistence/jdbc/AbstractProcessInstancesFactory.java
@@ -27,7 +27,7 @@ import org.kie.kogito.process.Processes;
 
 public abstract class AbstractProcessInstancesFactory implements ProcessInstancesFactory {
 
-    // test4
+    // test5
 
     private final DataSource dataSource;
     private final Boolean lock;
