120596c066d56dbc78c7aa95dd8d9d8c7b113b0b
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2026-09-08 12:06:17 +0900

    retrigger

--- kogito-apps-quarkus/integration-tests-jobs-service-quarkus/integration-tests-jobs-service-quarkus-management/src/test/java/org/kie/kogito/it/jobs/IndependentJobsIT.java
+++ kogito-apps-quarkus/integration-tests-jobs-service-quarkus/integration-tests-jobs-service-quarkus-management/src/test/java/org/kie/kogito/it/jobs/IndependentJobsIT.java
@@ -28,5 +28,5 @@ import io.quarkus.test.junit.QuarkusIntegrationTest;
 @JobServiceTestResource(kafkaEnabled = true, dataIndexEnabled = true)
 class IndependentJobsIT extends BaseIndependentJobsIT {
 
-    // Parallel CI sparse partitioning test06.
+    // Parallel CI sparse partitioning test06. retrigger
 }
